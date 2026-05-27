190aa03532f1093c153c322acd613a905f0f5bfb
Gladys Woo Hui Min <gladyswoo@192.168.1.8>
2026-05-27 19:00:41 +0800

    initial setup

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListsScreen from './screens/ListsScreen';
+import TodoScreen from './screens/TodoScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Lists">
+        <Stack.Screen name="Lists" component={ListsScreen} options={{ title: 'My Lists' }} />
+        <Stack.Screen name="Todos" component={TodoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/listsummary.js
@@ -0,0 +1,1 @@
+//shows the different lists of to dos
